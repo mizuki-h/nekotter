0a79de04270d3be44a50aabac79126af07b98550
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-04-26 17:25:35 +0900

    initial commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+  def new
+  end
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def tweet_params
+    params.permit(:text)
+  end
+end
